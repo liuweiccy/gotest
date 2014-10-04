2815b2b0b38d2b263512a0f84c484be3b069d13c
EricLiu <liuweiccy@users.noreply.github.com>
2014-10-04 23:48:21 +0800

    方法练习

---
+++ gotest1/src/methods-continued.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type MyFloat float64
+
+//对float64类型的求绝对值
+func (f MyFloat)Abs() float64{
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	f:=MyFloat(-math.Sqrt2)
+	fmt.Print(f.Abs())
+}
+
+
